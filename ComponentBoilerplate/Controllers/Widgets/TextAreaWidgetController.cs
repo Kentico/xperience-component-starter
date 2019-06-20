7fa9ca340847368ec0ddab5407d916c3aa4a0fba
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-06-20 08:40:05 +0200

    add first widget

--- ComponentBoilerplate/Controllers/Widgets/TextAreaWidgetController.cs
+++ ComponentBoilerplate/Controllers/Widgets/TextAreaWidgetController.cs
@@ -1,6 +1,19 @@
-﻿namespace Kentico.ComponentBoilerplate.Controllers.Widgets
+﻿using System.Web.Mvc;
+
+using Kentico.ComponentBoilerplate.Controllers.Widgets;
+using Kentico.PageBuilder.Web.Mvc;
+
+[assembly: RegisterWidget("OndrejChrastina.Widgets.TextAreaWidget", typeof(TextAreaWidgetController), "Text Area Widget", Description = "Displays a text deined in the text area.", IconClass = "icon-l-header-text")]
+
+
+namespace Kentico.ComponentBoilerplate.Controllers.Widgets
 {
-    public class TextAreaWidgetController
+    public class TextAreaWidgetController : WidgetController
     {
+        // GET: TextAreaWidget
+        public ActionResult Index()
+        {
+            return PartialView("Widgets/_TextAreaWidget");
+        }
     }
 }
